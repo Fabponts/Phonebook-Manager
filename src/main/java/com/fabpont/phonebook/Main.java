576581e4dc87722ca07247d2030393ba2b783f47
Fabpont <fabriciopontes02.fa@gmail.com>
2025-08-12 21:53:43 -0300

    chore: add new packages:controller,dto,entities,util and view to organize the project

--- src/main/java/com/fabpont/phonebook/Main.java
+++ src/main/java/com/fabpont/phonebook/Main.java
@@ -1,9 +1,16 @@
 package com.fabpont.phonebook;
 
+
+import com.fabpont.phonebook.view.MenuView;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        MenuView menuView = new MenuView();
+
+        //Menus
+        menuView.InroductionView();
         }
 
     }
--- src/main/java/com/fabpont/phonebook/view/MenuView.java
+++ src/main/java/com/fabpont/phonebook/view/MenuView.java
@@ -1,24 +1,51 @@
 package com.fabpont.phonebook.view;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
+
 
 public class MenuView {
+
+    //Objects instantiation
     private Scanner scanner = new Scanner(System.in);
+    private Logger logger = Logger.getLogger(MenuView.class.getName());
+
+    // Class Variable
     private Integer option;
 
-    public MenuView() {
-        System.out.println("------Welcome to your Phonebook-------");
-        System.out.println("------Witch action do you want to do?------");
+    // Class MenuView
+     public void IntroductionView(){
+       logger.info("------Welcome to your Phonebook-------");
+       logger.info("------Which action do you want to do?");
+       logger.info("1: Go to main menu");
+       logger.info("2: Exit ");
 
-        while (option != 1 || option != 2) {
-            System.out.println("Please enter a valid option");
+        option = scanner.nextInt();
+
+        // Verifying wrong options
+        while (option != 1 && option != 2) {
+            logger.warning("Please enter a valid option");
+            option = scanner.nextInt();
         }
 
-        if (option == 1){
+        //
+        switch(option){
+            case 1:
+                Menu();
+                break;
 
+                case 2:
+                    System.exit(0);
         }
-        else if (option == 2){
-            System.exit(0);
-        }
+    }
+    public void Menu(){
+        System.out.println("1: Add a new contact");
+        System.out.println("2: Show contacts");
+        System.out.println("3: Delete a contact");
+        System.out.println("4: Edit a contact");
+        System.out.println("5: Search for a contact");
+        System.out.println("6: Reset your phonebook");
+
+        scanner.close();
     }
 }
